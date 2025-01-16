8c83987f5a1bba10bedda13a123cb2bb4207a953
cuculeha <86766249+cuculeha@users.noreply.github.com>
2025-01-16 13:08:03 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+// Author: Saliha Babar
+// Assignment: A2
+// Due Date: 1/21/2025
+// Purpose: C-Style Arrays
+
+
 #include <iostream>
 using namespace std;
 
@@ -8,6 +14,7 @@ const int numChars = 80;
 int main(){
   char bookTitle[numBooks][numChars] = {"Hello", "Saliha", "Popular Books"};
 
+  
 
 cout << "Hello World.. Project A2!" << endl;
 return 0;
