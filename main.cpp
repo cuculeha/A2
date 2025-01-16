c1e075c5dae18b4d30603e2a83828688135db898
cuculeha <86766249+cuculeha@users.noreply.github.com>
2025-01-16 12:58:27 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
+
+const int numBooks = 20;
+const int numChars = 80;
 int main(){
+  char bookTitle[numBooks][numChars];
+
+  
 cout << "Hello World.. Project A2!" << endl;
 return 0;
 }
